81f12e0f01924ad6c71643e6f38b08e59bf524be
unknown <simovicmilica67@gmail.com>
2021-07-17 21:40:47 +0200

    upload

--- react/my-app/src/Blogs.js
+++ react/my-app/src/Blogs.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 
+import './Blogs.css';
 
-function Blogs(){
-    return(
-        <div>
-            <h1>Blogs</h1>
+function Blogs() {
+  const [posts, setPosts] = useState();
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const getPosts = async () => {
+    const response = await Axios.get('https://jsonplaceholder.typicode.com/posts');
+
+    console.log('Blog posts:', response);
+
+    setPosts(response);
+  };
+
+  return (
+    <div className='blogs'>
+        <h1>Blogs</h1>
+      {posts && posts.data.map((post, index) => (
+        <div
+          className='post'
+
+          key={index}
+        >
+          <h1>{post.title}</h1>
+
+          <p>{post.body}</p>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 export default Blogs;
