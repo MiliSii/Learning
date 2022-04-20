fecc00f7d83bbed5a9bc3f1ebec43cb3ce9cee36
unknown <simovicmilica67@gmail.com>
2022-04-20 18:22:02 +0200

    update

--- nineball/my-app/src/component/common/Slider/Slider.js
+++ nineball/my-app/src/component/common/Slider/Slider.js
@@ -7,15 +7,42 @@ import { sliderData } from "./slider_data";
 
 const Slider =()=>{
     const[currentSlide,setCurrentSlide]=useState(0);
-    const slideLenghth=sliderData.length;
+    const slideLength=sliderData.length;/* slideLength=1 2 3 ako iskoristimo console.log  */
+    //currentSlide=0 1 2  to su pozicije 
+    
+    const autoScroll=true;
+    let slideInterval;
+    let intervalTime=5000;
+
+    const nextSlide=()=>{
+        setCurrentSlide(currentSlide===slideLength -1?0:currentSlide +1);
+
+    };
+
+    const prevSlide=()=>{
+        setCurrentSlide(currentSlide===0?slideLength -1:currentSlide -1);
+
+    };
+
+    function auto(){
+        slideInterval=setInterval(nextSlide,intervalTime);
+    }
+    
     useEffect(()=>{
-        setCurrentSlide(0)
-    },[])
+        setCurrentSlide(0);
+    },[]);
+
+    useEffect(()=>{
+        if(autoScroll){
+            auto();
+        }
+        return()=>clearInterval(slideInterval);
+    },[currentSlide]);
     
     return(
         <div className="slider">
-            <AiOutlineArrowLeft className="arrow prev"/>
-            <AiOutlineArrowRight className="arrow next"/>
+            <AiOutlineArrowLeft className="arrow prev" onClick={prevSlide}/>
+            <AiOutlineArrowRight className="arrow next" onClick={nextSlide}/>
 
             {sliderData.map((slide, index)=>{
                 return(
